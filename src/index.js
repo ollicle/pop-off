e6054eb18892deba3cfeac0a2cd111400acc32dc
Oliver Boermans <ollie@ollicle.com>
2018-06-19 20:43:29 +0930

    New: add stripes to overlay

--- src/index.js
+++ src/index.js
@@ -11,6 +11,8 @@ catcher.style.bottom = '0';
 catcher.style.right = '0';
 catcher.style.zIndex = '2147483647';
 catcher.style.cursor = 'crosshair';
+catcher.style.background = 'linear-gradient(135deg, rgba(255,255,255,0.2) 25%, rgba(0,0,0,0) 25%, rgba(0,0,0,0) 50%, rgba(255,255,255,0.2) 50%, rgba(255,255,255,0.2) 75%, rgba(0,0,0,0) 75%, rgba(0,0,0,0) 0), linear-gradient(135deg, rgba(0,0,0,0.1) 0, rgba(0,0,0,0.1) 24%, rgba(0,0,0,0) 26%, rgba(0,0,0,0) 50%, rgba(0,0,0,0.1) 50%, rgba(0,0,0,0.1) 75%, rgba(255,255,255,0) 75%, rgba(0,0,0,0) 100%)';
+catcher.style.backgroundSize = '2rem 2rem';
 
 catcher.addEventListener('click', function(e){
 	e.stopPropagation();
